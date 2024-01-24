c58f70af63f39ff9c6d9ee24c4e14e3dcd1ff202
jgray-dev <nohaxjustdoge@gmail.com>
2024-01-24 15:02:12 -0700

    nv1 // 3:02pm

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+function writeCards(arr) {
+    let result = [];
+    for (let i = 0; i < arr.length; i++) {
+        result[i] = `Thank you, ${arr[i]}, for the wonderful surprise gift!`;
+    }
+    return result;
+};
+
+
+function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+        console.log(i);
+    }
+};
